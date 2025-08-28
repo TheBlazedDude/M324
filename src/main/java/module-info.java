b64af8f15f97da21fd5c61a39e4dad20a8ca33f2
theblazeddude <bene.brueck@gmx.ch>
2025-08-28 14:08:19 +0200

    fixed javaFX graphics dependency

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module ch.bbw.BenBrc {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+    requires javafx.base;
+    
+    exports ch.bbw.BenBrc;
+    exports ch.bbw.BenBrc.model;
+    exports ch.bbw.BenBrc.model.operations;
+    exports ch.bbw.BenBrc.module;
+}
